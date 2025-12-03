7ef26664bdc6cc5cdad8d8090ab75e672f3f7629
Jhony Freitas <jhony00._@hotmail.com>
2025-12-03 14:14:27 -0300

    fix: Fixing type

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,37 +1,39 @@
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
 import { ICreateUser, IUpdateUserRequest } from "../interfaces/user";
 
 export class UserModel {
   private prisma: PrismaClient;
 
   constructor() {
-    this.prisma = new PrismaClient
+    this.prisma = new PrismaClient();
   }
 
-  async create ({company, user}: ICreateUser) {
-    return await this.prisma.$transaction(async (transaction) => {
-      const companyResult = await transaction.companies.create({
-        data: {
-          name: company.companyName,
-          tradingName: company.tradingName,
-          cnpj: company.cnpj,
-        }
-      })
+  async create({ company, user }: ICreateUser) {
+    return await this.prisma.$transaction(
+      async (transaction: Prisma.TransactionClient) => {
+        const companyResult = await transaction.companies.create({
+          data: {
+            name: company.companyName,
+            tradingName: company.tradingName,
+            cnpj: company.cnpj,
+          },
+        });
 
-      return await transaction.users.create({
-        data: {
-          ...user,
-          companyId: companyResult.id,
-        },
-      })
-    })
+        return await transaction.users.create({
+          data: {
+            ...user,
+            companyId: companyResult.id,
+          },
+        });
+      }
+    );
   }
 
-  async findAll ({id, email}: {id?: number, email?: string}) {
+  async findAll({ id, email }: { id?: number; email?: string }) {
     return await this.prisma.users.findMany({
       where: {
-        ...(id && {id}),
-        ...(email && {email}),
+        ...(id && { id }),
+        ...(email && { email }),
         deleted_at: null,
       },
       select: {
@@ -46,14 +48,14 @@ export class UserModel {
         role: true,
         updated_at: true,
         verified: true,
-      }
-    })
+      },
+    });
   }
 
   async update(id: number, data: IUpdateUserRequest) {
-   return await this.prisma.users.update({
+    return await this.prisma.users.update({
       where: { id },
-      data
-   }) 
+      data,
+    });
   }
 }
